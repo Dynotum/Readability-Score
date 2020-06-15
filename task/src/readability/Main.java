12d359d8eed2116e3481e7404fada259fea2fc61
Dynotum <dino_arquero_1@hotmail.com>
2020-06-14 19:22:37 -0500

    stage1 completed

--- task/src/readability/Main.java
+++ task/src/readability/Main.java
@@ -1,7 +1,17 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        final Scanner scanner = new Scanner(System.in);
+
+        final String text = scanner.nextLine();
+
+        if (text.length() <= 100) {
+            System.out.println("EASY");
+        } else {
+            System.out.println("HARD");
+        }
     }
 }
